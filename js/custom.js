60c0790035b94909f398f1c1929a671e23d9fe79
masaki goda <masa10@genius-2.local>
2016-11-07 00:32:01 +0900

    no message

--- js/custom.js
+++ js/custom.js
@@ -23,7 +23,8 @@
         //Device.js will check if it is Tablet or Mobile - http://matthewhudson.me/projects/device.js/
         if (!device.tablet() && !device.mobile()) {
             $(".player").mb_YTPlayer();
-        } else {
+        } 
+                      else {
             //jQuery will add the default background to the preferred class 
             $('.big-background, .small-background-section').addClass(
                 'big-background-default-image');
